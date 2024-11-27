36d198e72cee6f375553380a4f702a11661a62f7
Larva Vojtěch (246886) <246886@vutbr.cz>
2024-11-27 12:47:42 +0100

    Version 7 / 27.11.24 / Light detection with servos

--- src/main.c
+++ src/main.c
@@ -4,18 +4,28 @@
 #include <uart.h>
 #include <timer.h>
 #include <pwm.h>
+#include <gpio.h>
 
-#define ADC_CHANNEL 0
+#define UP_LDR_PIN 0
+#define DOWN_LDR_PIN 1
+#define LEFT_LDR_PIN 2
+#define RIGHT_LDR_PIN 3
+
+#define TOP 65400
+#define BOTTOM 50
 
 uint16_t ldr_light_detect;
 char string[8];
 
 void adc_init(void) {
-  ADMUX = (1 <<REFS0) | (ADC_CHANNEL & 0x07);
+  
   ADCSRA = (1 <<ADEN) | (1 <<ADPS2) | (1 <<ADPS1) | (1 <<ADPS0);
 }
 
-uint16_t adc_read(void) {
+uint16_t adc_read(uint8_t channel) {
+  ADMUX = (1 <<REFS0) | (channel & 0x07);
+
+
   ADCSRA |= (1 <<ADSC);
   while (ADCSRA & (1 <<ADSC));
   return ADC;
@@ -36,7 +46,7 @@ int main(void) {
 
   
   adc_init();
-  DDRB |= (1<<PB0);               //???????
+  DDRB |= (1<<PB5);               //???????
 
   while (1) {
     
@@ -49,10 +59,47 @@ ISR(TIMER0_OVF_vect)
     static uint8_t n_ovfs = 0;
 
     n_ovfs++;
-    if (n_ovfs >= 200) {
-      ldr_light_detect = adc_read();
+    if (n_ovfs >= 100) {
+      //flag_interrupt = 1;
+
+      ldr_light_detect = adc_read(UP_LDR_PIN);
+      uart_puts("UP: ");
       uart_puts(itoa(ldr_light_detect, string, 10));
       uart_puts("\n");
+
+      ldr_light_detect = adc_read(DOWN_LDR_PIN);
+      uart_puts("DOWN: ");
+      uart_puts(itoa(ldr_light_detect, string, 10));
+      uart_puts("\n");
+
+      ldr_light_detect = adc_read(LEFT_LDR_PIN);
+      uart_puts("LEFT: ");
+      uart_puts(itoa(ldr_light_detect, string, 10));
+      uart_puts("\n");
+
+      ldr_light_detect = adc_read(RIGHT_LDR_PIN);
+      uart_puts("RIGHT: ");
+      uart_puts(itoa(ldr_light_detect, string, 10));
+      uart_puts("\n");
+      uart_puts("\n");
+      GPIO_toggle(&PORTB,PB5);
+      n_ovfs = 0;
+
+      if ((adc_read(UP_LDR_PIN) < adc_read(DOWN_LDR_PIN)) && (OCR1B < TOP)) {
+        OCR1B += 50;
+
+      }
+      if (adc_read(UP_LDR_PIN) > adc_read(DOWN_LDR_PIN) && (OCR1B > BOTTOM)) {
+        OCR1B-=50;
+      }
+      
+      if (adc_read(LEFT_LDR_PIN) < adc_read(RIGHT_LDR_PIN) && (OCR1A < TOP)) {
+        OCR1A+=50;
+      }
+      if (adc_read(LEFT_LDR_PIN) > adc_read(RIGHT_LDR_PIN) && (OCR1A > BOTTOM)) {
+        OCR1A-=50;
+      }
     }
+
 }
 
