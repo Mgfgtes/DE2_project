d2fe7085b60805b1a76d410316b611fc308645d9
Mgfgtes <mate2002@seznam.cz>
2024-12-11 09:50:56 +0100

    Doxygen comments

--- lib/pwm/pwm.c
+++ lib/pwm/pwm.c
@@ -1,33 +1,89 @@
+/***********************************************************************
+ * 
+ * PWM library for AVR-GCC.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2024 Matej Matousek
+ * 
+ * This work is licensed under the terms of the MIT license.
+ *
+ **********************************************************************/
+
+
+/* Includes ----------------------------------------------------------*/
 #include "pwm.h"
 
+
+/* Function definitions ----------------------------------------------*/
+/*************************************************************************
+*  Function:    TIM1_pwm_init()
+*  Purpose:     Initialize Timer 1 for Fast PWM with frequency 50Hz.
+*  Returns:     none
+*************************************************************************/
 void TIM1_pwm_init(void){
-    TCCR1A &= ~(1 << COM1A0);                   //Compare Output Mode, Fast PWM Mode - Clear OC1A on Compare Match, set OC1A at BOTTOM (non-inverting mode)
+    /*
+    *   Compare Output Mode, Fast PWM Mode - Clear OC1A on Compare Match, set OC1A at BOTTOM (non-inverting mode)
+    */
+    TCCR1A &= ~(1 << COM1A0);
     TCCR1A |= (1 << COM1A1);
 
-    TCCR1A &= ~(1 << COM1B0);                   //Compare Output Mode, Fast PWM Mode - Clear OC1B on Compare Match, set OC1B at BOTTOM (non-inverting mode)
+    /*
+    *   Compare Output Mode, Fast PWM Mode - Clear OC1B on Compare Match, set OC1B at BOTTOM (non-inverting mode)
+    */
+
+    TCCR1A &= ~(1 << COM1B0);
     TCCR1A |= (1 << COM1B1);
 
+
+    /*
+    *   Configure Fast PWM Mode
+    */
     TCCR1A &= ~(1 << WGM10);
-    TCCR1A |= (1 << WGM11);                     //configure Fast PWM Mode 
+    TCCR1A |= (1 << WGM11);                      
     TCCR1B |= (1 << WGM13) | (1 << WGM12);
 
-    TCCR1B &= ~((1<<CS12) | (1<<CS10));         //clock selection
+
+    /*
+    *   Clock selection with prescaler
+    */
+    TCCR1B &= ~((1<<CS12) | (1<<CS10));
     TCCR1B |= (1 << CS11);
 
-    ICR1=39999;                                 //TOP of counter
+    /*
+    *   TOP of counter
+    */
+    ICR1=39999;                                
     
-    OCR1A = 2999;                               //default servo´s angle 
+    /*
+    *   Default servo´s angle
+    */
+    OCR1A = 2999;
     OCR1B = 2999;
 
 }
 
-void TIM1_pwm_compA_value(uint8_t val){          
+
+/**********************************************************************
+ * Function: TIM1_pwm_compA_value()
+ * Purpose:  Set pulse width to specific value by Comparator 0 of Timer 1.
+ * Input(s): val - Specific number between 0 and 3999 where increse by 1 represents 0,5 us.
+ * Returns:  none
+ **********************************************************************/
+void TIM1_pwm_compA_value(uint16_t val){          
 
     OCR0A = val;
 
 }
 
-void TIM1_pwm_compB_value(uint8_t val){          
+
+/**********************************************************************
+ * Function: TIM1_pwm_compB_value()
+ * Purpose:  Set pulse width to specific value by Comparator 0 of Timer 1.
+ * Input(s): val - Specific number between 0 and 3999 where increse by 1 represents 0,5 us.
+ * Returns:  none
+ **********************************************************************/
+void TIM1_pwm_compB_value(uint16_t val){          
 
     OCR0B = val;
 
--- lib/pwm/pwm.h
+++ lib/pwm/pwm.h
@@ -1,12 +1,63 @@
+#ifndef PWM_H
+# define PWM_H
+
+/***********************************************************************
+ * 
+ * PWM library for AVR-GCC.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2024 Matej Matousek
+ * 
+ * This work is licensed under the terms of the MIT license.
+ *
+ **********************************************************************/
+
+/**
+ * @file 
+ * @defgroup pwm PWM Library <pwm.h>
+ * @code #include <pwm.h> @endcode
+ *
+ * @brief PWM library for AVR-GCC.
+ *
+ * The library contains functions for controlling Timer's 1 pin(s) with PWM.
+ *
+ * @note Based on AVR Libc Reference Manual. Tested on ATmega328P 
+ *       (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2.
+ * @author Matej Matousek, Brno University of Technology, Czechia
+ * @copyright (c) 2024 Matej Matousek, This work is licensed under 
+ *                the terms of the MIT license
+ * @{
+ */
+
+/* Includes ----------------------------------------------------------*/
 #include <avr/io.h>         // AVR device-specific IO definitions
 
+
+/* Function prototypes -----------------------------------------------*/
+/**
+ * @brief  Initialize Timer 1 for Fast PWM with frequency 50Hz.
+ * @return none
+ */
 void TIM1_pwm_init(void);
 
-void TIM1_pwm_compA_value(uint8_t val);
+/**
+ * @brief    Set pulse width to specific value by Comparator 0 of Timer 1
+ *
+ * @param    value Specific number between 0 and 3999 where increse by 1 represents 0,5 us.
+ * @return   none
+ */
+void TIM1_pwm_compA_value(uint16_t val);
 
-void TIM1_pwm_compB_value(uint8_t val);
+/**
+ * @brief    Set pulse width to specific value by Comparator 1 of Timer 1
+ *
+ * @param    value Specific number between 0 and 3999 where increse by 1 represents 0,5 us.
+ * @return   none
+ */
+void TIM1_pwm_compB_value(uint16_t val);
 
-
+#endif
 
 
 
